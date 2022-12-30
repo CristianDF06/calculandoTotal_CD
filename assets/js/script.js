9a95be3f7075de828b2493ed6807cc4e696b2555
Cristian <duranferreira.cristian@gmail.com>
2022-12-30 03:41:13 -0300

    desafio01_m3

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+var precio = 400000;
+
+var cantidadInput = document.querySelector("input[type='number']");
+
+var colorInput = document.querySelector("input[type='text']");
+
+//Totales
+
+var precioFinal = document.querySelector(".valor-total");
+
+var cantidadFinal = document.querySelector(".cantidad-total");
+
+var colorFinal = document.querySelector(".color div");
+
+function calcularTotal () {
+    var cantidad = cantidadInput.value;
+    var color = colorInput.value;
+
+    precioFinal.innerHTML = (precio * cantidad).toLocaleString();
+    cantidadFinal.innerHTML = cantidad;
+    colorFinal.style.backgroundColor = color;
+}
